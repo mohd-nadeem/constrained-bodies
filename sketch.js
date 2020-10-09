45cd9248bbba0b6e8b4c4e23870284dc6a258c7c
Mohd. Nadeem Sheikh <mohd.nadeem.sheikh24@gmail.com>
2020-10-09 14:44:19 +0530

    completed

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var log6;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -36,6 +37,9 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    log6 = new Log(230, 180, 80, PI/2);
+    chain = new Chain(log6.body, bird.body);
+
 }
 
 function draw(){
@@ -61,4 +65,6 @@ function draw(){
 
     bird.display();
     platform.display();
+    chain.display();
+    log6.display();
 }
